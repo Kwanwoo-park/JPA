e6da9634f9a6d8993cd7b2afc891122bdff41cbd
akakslslzz@naver.com <akakslslzz@naver.com>
2024-05-23 23:02:06 +0900

    챕터5 진행중

---
+++ src/main/java/org/example/Chapter5/Main.java
@@ -0,0 +1,91 @@
+package org.example.Chapter5;
+
+import org.example.entity.Chapter5.Member;
+import org.example.entity.Chapter5.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
+
+public class Main {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    static EntityManager em = emf.createEntityManager();
+    static EntityTransaction tx = em.getTransaction();
+
+    public static void main(String[] args) {
+        try {
+            tx.begin();
+            testSave();
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    static void testSave() {
+        Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
+
+        Member member1 = new Member("member1", "회원1");
+        member1.setTeam(team1);
+        em.persist(member1);
+
+        Member member2 = new Member("member2", "회원2");
+        member2.setTeam(team1);
+        em.persist(member2);
+
+        Member member = em.find(Member.class, "member1");
+        Team team = member.getTeam();
+        System.out.println("팀 이름 = " + team.getName());
+
+//        queryLogicJoin();
+//
+//        updateRelation();
+//
+//        deleteRelation();
+
+        biDirection();
+    }
+
+    private static void queryLogicJoin() {
+        String jpql = "select m from Member m join m.team t where t.name = :teamName";
+
+        List<Member> resultList = em.createQuery(jpql, Member.class)
+                .setParameter("teamName", "팀1")
+                .getResultList();
+
+        for (Member member : resultList) {
+            System.out.println("[query] member.username = " + member.getUsername());
+        }
+    }
+
+    private static void updateRelation() {
+        Team team2 = new Team("team2", "팀2");
+        em.persist(team2);
+
+        Member member = em.find(Member.class, "member1");
+        member.setTeam(team2);
+    }
+
+    private static void deleteRelation() {
+        Member member1 = em.find(Member.class, "member1");
+        member1.setTeam(null);
+    }
+
+    public static void biDirection() {
+        Team team = em.find(Team.class, "team1");
+        List<Member> members = team.getMembers();
+
+        for (Member member : members) {
+            System.out.println("member.username = " + member.getUsername());
+        }
+    }
+}
